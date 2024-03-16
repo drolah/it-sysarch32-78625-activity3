0d4b8423fac6e5108aebbfc5573b5fe7b063061a
drolah <akiraisabelle12@gmail.com>
2024-03-16 22:22:13 +0800

    add

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -3,10 +3,7 @@ import Pokemon from "./Pokemon";
 
 function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
-  
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [selectedLanguage, setSelectedLanguage] = useState("english");
 
   const fetchData = () => {
     fetch("https://us-central1-it-sysarch32.cloudfunctions.net/pokemon")
@@ -19,36 +16,39 @@ function Pokedex() {
       });
   };
 
-  const LanguageEnglish = () => {
-    return (
-      <div className="pokemons">
-        {pokemons.map(pokemon => (
-          <Pokemon
-            key={pokemon.id}
-            id={pokemon.id}
-            name={pokemon.name.english}
-            types={pokemon.type}
-            hp={pokemon.base.HP}
-            attack={pokemon.base.Attack}
-            defense={pokemon.base.Defense}
-            speed={pokemon.base.Speed}
-            specialAttack={pokemon.base["Sp. Attack"]}
-            specialDefense={pokemon.base["Sp. Defense"]}
-            image={pokemon.image}
-          />
-        ))}
-      </div>
-    );
-  };
+  useEffect(() => {
+    fetchData(selectedLanguage);
+  }, [selectedLanguage]);
 
   return (
     <div>
       <div>
-        <button className="button" onClick={fetchData}>English</button>
+        <button className="button" onClick={() => setSelectedLanguage("english")}>English</button>
+        <button className="button" onClick={() => setSelectedLanguage("japanese")}>Japanese</button>
+        <button className="button" onClick={() => setSelectedLanguage("chinese")}>Chinese</button>
+        <button className="button" onClick={() => setSelectedLanguage("french")}>French</button>
       </div>
-      <LanguageEnglish />
+      {
+        <div className="pokemons">
+          {pokemons.map(pokemon => (
+            <Pokemon
+              key={pokemon.id}
+              id={pokemon.id}
+              name={pokemon.name[selectedLanguage]} // Dynamically select name based on selected language
+              types={pokemon.type}
+              hp={pokemon.base.HP}
+              attack={pokemon.base.Attack}
+              defense={pokemon.base.Defense}
+              speed={pokemon.base.Speed}
+              specialAttack={pokemon.base["Sp. Attack"]}
+              specialDefense={pokemon.base["Sp. Defense"]}
+              image={pokemon.image}
+            />
+          ))}
+        </div>
+      }
     </div>
-  );
+  )
 }
 
-export default Pokedex;
+export default Pokedex
